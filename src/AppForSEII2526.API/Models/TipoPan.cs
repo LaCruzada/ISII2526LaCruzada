22868a5bfdd2874626788a6891b5ba3248a096cc
JoseJavierML <josejavier.maestro@alu.uclm.es>
2025-10-03 08:44:05 +0200

    Añadidas clases para gestión de bocadillos y compras
    Se han añadido las siguientes clases y enumerado para modelar
    un sistema de gestión de bocadillos, compras y métodos de pago:
    
    - Clase `Bocadillo` en `Bocadillo.cs` con propiedades y
      relaciones con `TipoPan` y `CompraBocadillo`.
    - Clase `CompraBocadillo` en `ComprarBocadillo.cs` para
      representar la relación entre compras y bocadillos.
    - Enumerado `Metodo_Pago` y clase `Compra` en `Metodo_Pago.cs`
      para gestionar métodos de pago y detalles de compras.
    - Clase `TipoPan` en `TipoPan.cs` para representar tipos de pan.
    
    Se han aplicado validaciones y configuraciones para la integración
    con la base de datos, incluyendo atributos como `[Key]`, `[Required]`
    y `[Column]`.

---
+++ src/AppForSEII2526.API/Models/TipoPan.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.Models
+{
+    public class TipoPan
+    {
+        [Key]
+        public int PanId { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public ICollection<Bocadillo> Bocadillos { get; set; }
+    }
+}
