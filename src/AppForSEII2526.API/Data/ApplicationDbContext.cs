87d094112a26f5457950f6e1a3564331f5bf00a2
Mario Navarro de la Cruz <mario.navarro4@alu.uclm.es>
2025-10-04 17:43:46 +0200

    4 crear reseña (#16)
    * Creación de clases y cambio de ApplicationDbContext
    
    * Cambios en las restricciones de Bocadillo y Reseña
    
    * Solución errores
    
    * Solución errores y cambios de nombre en el atributo PanId de Bocadillo a TipoPanId

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -18,16 +18,17 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<BonoBocadillo> BonoBocadillo { get; set; }
     public DbSet<BonosComprados> BonosComprados { get; set; }
     public DbSet<CompraBono> CompraBono { get; set; }
-
+    public DbSet<Bocadillo> Bocadillos { get; set; }
+    public DbSet<TipoPan> TipoPanes { get; set; }
+    public DbSet<Resenya> Resenyas { get; set; }
+    public DbSet<ResenyaBocadillo> ResenyaBocadillos { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
         // Configuración de clave compuesta para ProductoCompra
         builder.Entity<ProductoCompra>().HasKey(pc => new { pc.CompraId, pc.ProductoId });
-    }
         builder.Entity<BonosComprados>().HasKey(pi => new { pi.compraBonoId, pi.bonoId });
+        builder.Entity<ResenyaBocadillo>().HasKey(pc => new { pc.BocadilloId, pc.ResenyaId });
     }
-
-
 }
---
+++ src/AppForSEII2526.API/Models/Resenya.cs
@@ -0,0 +1,59 @@
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Resenya
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(200, ErrorMessage = "La descripción no puede tener más de 200 caracteres.", MinimumLength = 1)]
+        public string? descripcion { get; set; }
+
+        [DataType(DataType.DateTime), Display(Name = "Fecha Reseña")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaInicio { get; set; }
+
+        [StringLength(20, ErrorMessage = "El nombre no puede ocupar más de 20 caracteres.", MinimumLength = 1)]
+        public string? NombreUsuario { get; set; }
+
+        [StringLength(20, ErrorMessage = "El titulo no puede ocupar más de 20 caracteres.", MinimumLength = 1)]
+        public string? Titulo { get; set; }
+
+        public EnumValoracion_General Valoracion_General { get; set; }
+
+        public List<ResenyaBocadillo> ResenyaBocadillos { get; set; } = new List<ResenyaBocadillo>();
+
+        public Resenya()
+        {
+            ResenyaBocadillos = new List<ResenyaBocadillo>();
+        }
+        public Resenya(int id, string descripcion, DateTime fechaInicio, string nombreUsuario, string titulo, EnumValoracion_General valoracion_General, List<ResenyaBocadillo> resenyaBocadillo)
+        {
+            Id = id;
+            this.descripcion = descripcion;
+            FechaInicio = fechaInicio;
+            NombreUsuario = nombreUsuario;
+            Titulo = titulo;
+            Valoracion_General = valoracion_General;
+            ResenyaBocadillos = resenyaBocadillo;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Resenya resenya &&
+                   Id == resenya.Id &&
+                   descripcion == resenya.descripcion &&
+                   FechaInicio == resenya.FechaInicio &&
+                   NombreUsuario == resenya.NombreUsuario &&
+                   Titulo == resenya.Titulo &&
+                   Valoracion_General == resenya.Valoracion_General &&
+                   EqualityComparer<List<ResenyaBocadillo>>.Default.Equals(ResenyaBocadillos, resenya.ResenyaBocadillos);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, descripcion, FechaInicio, NombreUsuario, Titulo, Valoracion_General, ResenyaBocadillos);
+        }
+    }
+}
