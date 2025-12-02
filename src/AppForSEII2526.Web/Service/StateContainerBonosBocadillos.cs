4dbad7090ba856ff89bf7507c72ac87442329b85
Samuel Notario Sáez <samuel.notario@alu.uclm.es>
2025-12-02 16:17:54 +0100

    #113 (#114)
    Añadido DTO y contenedor de estado para compra de bonos
    
    Se ha añadido el archivo `BonosSelectDTO.cs` con la clase
    `BonoSelectDTO` para representar los bonos en el carrito.
    
    Se ha creado la clase `ComprarBonoStateContainer` en
    `StateContainerBonosBocadillos.cs` para gestionar el estado
    de la compra de bonos, incluyendo métodos para agregar,
    eliminar y limpiar el carrito, así como procesar la compra.

---
+++ src/AppForSEII2526.Web/Service/StateContainerBonosBocadillos.cs
@@ -0,0 +1,97 @@
+﻿using AppForSEII2526.API.ComprarBonoBocadilloDTOs;
+using AppForSEII2526.API.DTOs.CompraBonoDTOs;
+using AppForSEII2526.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppForSEII2526.Web.Services
+{
+    public class ComprarBonoStateContainer
+    {
+        public ComprarBonoBocadilloPost Compra { get; private set; } = new ComprarBonoBocadilloPost
+        {
+            NombreCliente = "",
+            ApellidoCliente1 = "",
+            ApellidoCliente2 = "",
+            pago = MetodoPago.Tarjeta,
+            FechaCompra = DateTime.Now,
+            BonoItem = new List<BonosCompradosDTO>()
+        };
+
+        public List<BonoSelectDTO> CarritoVisual { get; private set; } = new List<BonoSelectDTO>();
+
+        public double PrecioTotal => Compra.PrecioTotal;
+
+        public event Action? OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AgregarBono(BonoSelectDTO bonoSeleccionado)
+        {
+            CarritoVisual.Add(bonoSeleccionado);
+
+            var itemExistente = Compra.BonoItem.FirstOrDefault(b => b.BonoID == bonoSeleccionado.BonoId);
+
+            if (itemExistente != null)
+            {
+                itemExistente.Cantidad++;
+            }
+            else
+            {
+                Compra.BonoItem.Add(new BonosCompradosDTO
+                {
+                    BonoID = bonoSeleccionado.BonoId,
+                    Nombre = bonoSeleccionado.Nombre,
+                    PrecioUnitario = bonoSeleccionado.PVP,
+                    Cantidad = 1,
+                    Tipo = bonoSeleccionado.TipoBocadillo
+                });
+            }
+
+            NotifyStateChanged();
+        }
+
+        public void EliminarBono(BonoSelectDTO bonoSeleccionado)
+        {
+            CarritoVisual.Remove(bonoSeleccionado);
+
+            var itemExistente = Compra.BonoItem.FirstOrDefault(b => b.BonoID == bonoSeleccionado.BonoId);
+
+            if (itemExistente != null)
+            {
+                itemExistente.Cantidad--;
+                if (itemExistente.Cantidad <= 0)
+                {
+                    Compra.BonoItem.Remove(itemExistente);
+                }
+            }
+
+            NotifyStateChanged();
+        }
+
+        public void LimpiarCarrito()
+        {
+            Compra.BonoItem.Clear();
+            CarritoVisual.Clear();
+            NotifyStateChanged();
+        }
+
+        public void CompraProcesada()
+        {
+            Compra = new ComprarBonoBocadilloPost
+            {
+                NombreCliente = "",
+                ApellidoCliente1 = "",
+                ApellidoCliente2 = "",
+                pago = MetodoPago.Tarjeta,
+                FechaCompra = DateTime.Now,
+                BonoItem = new List<BonosCompradosDTO>()
+            };
+            CarritoVisual = new List<BonoSelectDTO>();
+
+            NotifyStateChanged();
+        }
+    }
+
+
+}
