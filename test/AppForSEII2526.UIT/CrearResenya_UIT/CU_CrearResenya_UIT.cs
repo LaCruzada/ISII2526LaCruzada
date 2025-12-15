0073f79964c5f62e884787cdb9f01cad0ab08ecc
Mario Navarro de la Cruz <Mario.Navarro4@alu.uclm.es>
2025-12-15 20:57:39 +0100

    Cambios y creación de pruebas UIT

---
+++ test/AppForSEII2526.UIT/CrearResenya_UIT/CU_CrearResenya_UIT.cs
@@ -0,0 +1,151 @@
+﻿using AppForMovies.UIT.Shared;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+using AppForSEII2526.UIT.Resenya_UIT;
+using System.Threading;
+
+namespace AppForSEII2526.UIT.CrearResenya_UIT
+{
+    public class CU_CrearResenya_UIT : UC_UIT
+    {
+        private const string bocadillo1Nombre = "Bocadillo Vegano";
+        private const string bocadillo2Nombre = "Bocadillo Sin Gluten";
+
+        private const string usuarioNombre = "Ana";
+        private const string tituloResenya = "¡Excelente!";
+        private const string descripcionResenya = "Muy sabroso y recomendable.";
+        private const string valoracionGeneral = "5";
+
+        public CU_CrearResenya_UIT(ITestOutputHelper output) : base(output) { }
+
+        private void Ir_A_CrearResenya()
+        {
+            _driver.Navigate().GoToUrl(_URI + "Resenya/Select");
+            
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        [Trait("CasoPrueba", "ESC-01")]
+        public void ESC_01_Crear_Resenya_Estandar()
+        {
+            var selectPO = new SelectBocadillosResenya_PO(_driver, _output);
+            var postPO = new PostCrearResenya_PO(_driver, _output);
+            var detallesPO = new DetailsResenya_PO(_driver, _output);
+
+            Ir_A_CrearResenya();
+
+            selectPO.AddBocadilloByName(bocadillo1Nombre);
+            selectPO.ClickCrearResenya();
+
+            postPO.SetNombreUsuario(usuarioNombre);
+            postPO.SetTituloResenya(tituloResenya);
+            postPO.SetDescripcionResenya(descripcionResenya);
+            postPO.SetValoracionGeneral(valoracionGeneral);
+            postPO.SetPuntuacionBocadillo("1", "9"); // Ejemplo ID bocadillo
+
+            postPO.PublicarResenya();
+            detallesPO.WaitForBeingVisible(By.Id("ResumenResenya"));
+
+            Assert.Contains(usuarioNombre, _driver.PageSource);
+            Assert.Contains(tituloResenya, _driver.PageSource);
+            Assert.Contains(descripcionResenya, _driver.PageSource);
+            Assert.Contains(bocadillo1Nombre, _driver.PageSource);
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        [Trait("CasoPrueba", "ESC-02")]
+        public void ESC_02_Filtrar_Bocadillos()
+        {
+            var selectPO = new SelectBocadillosResenya_PO(_driver, _output);
+
+            Ir_A_CrearResenya();
+            selectPO.SearchBocadillos("Vegano", null, null);
+
+            Assert.True(selectPO.CheckListOfBocadillos(new List<string> { bocadillo1Nombre }));
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        [Trait("CasoPrueba", "ESC-03")]
+        public void ESC_03_Carrito_Vacio_Boton_Deshabilitado()
+        {
+            var selectPO = new SelectBocadillosResenya_PO(_driver, _output);
+
+            Ir_A_CrearResenya();
+            Assert.True(selectPO.CrearResenyaNotAvailable());
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        [Trait("CasoPrueba", "ESC-04")]
+        public void ESC_04_Eliminar_Bocadillo_Carrito()
+        {
+            var selectPO = new SelectBocadillosResenya_PO(_driver, _output);
+
+            Ir_A_CrearResenya();
+
+            selectPO.AddBocadilloByName(bocadillo1Nombre);
+            selectPO.AddBocadilloByName(bocadillo2Nombre);
+            selectPO.RemoveBocadilloFromCart(bocadillo1Nombre);
+
+            Assert.True(selectPO.IsBocadilloInCart(bocadillo2Nombre));
+            Assert.False(selectPO.IsBocadilloInCart(bocadillo1Nombre));
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        [Trait("CasoPrueba", "ESC-05")]
+        public void ESC_05_Datos_Obligatorios()
+        {
+            var selectPO = new SelectBocadillosResenya_PO(_driver, _output);
+            var postPO = new PostCrearResenya_PO(_driver, _output);
+
+            Ir_A_CrearResenya();
+
+            selectPO.AddBocadilloByName(bocadillo1Nombre);
+            selectPO.WaitForBeingClickable(By.Id("CrearResenya"));
+            _driver.FindElement(By.Id("CrearResenya")).Click();
+            Thread.Sleep(1000);
+
+            postPO.SetDescripcionResenya(descripcionResenya);
+            postPO.PublicarResenya();
+
+            Assert.True(postPO.CheckMessageError("obligatorio"));
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        [Trait("CasoPrueba", "ESC-06")]
+        public void ESC_06_Modificar_Bocadillos_Mantiene_Datos()
+        {
+            var selectPO = new SelectBocadillosResenya_PO(_driver, _output);
+            var postPO = new PostCrearResenya_PO(_driver, _output);
+
+            Ir_A_CrearResenya();
+
+            selectPO.AddBocadilloByName(bocadillo1Nombre);
+            selectPO.WaitForBeingClickable(By.Id("CrearResenya"));
+            _driver.FindElement(By.Id("CrearResenya")).Click();
+            Thread.Sleep(1000);
+
+            postPO.SetNombreUsuario(usuarioNombre);
+            postPO.SetTituloResenya(tituloResenya);
+
+            postPO.ModificarBocadillos();
+            Thread.Sleep(1000);
+
+            Assert.True(_driver.Url.Contains("/Resenya/Select"));
+            Assert.True(selectPO.IsBocadilloInCart(bocadillo1Nombre));
+
+            _driver.FindElement(By.Id("CrearResenya")).Click();
+            Thread.Sleep(1000);
+
+            var tituloInput = _driver.FindElement(By.Id("TituloResenya"));
+            Assert.Equal(tituloResenya, tituloInput.GetAttribute("value"));
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/CrearResenya_UIT/PostCrearResenya.cs
@@ -0,0 +1,130 @@
+﻿using OpenQA.Selenium;
+using AppForSEII2526.UIT.Shared;
+using Xunit.Abstractions;
+using OpenQA.Selenium.Support.UI;
+
+namespace AppForSEII2526.UIT.Resenya_UIT
+{
+    public class PostCrearResenya_PO : PageObject
+    {
+
+
+        private By _nombreUsuarioBy = By.Id("NombreUsuario");
+        private By _tituloResenyaBy = By.Id("TituloResenya");
+        private By _descripcionResenyaBy = By.Id("DescripcionResenya");
+        private By _valoracionGeneralBy = By.Id("ValoracionGeneral");
+
+        private string _puntuacionBocadilloPrefix = "Puntuacion_";
+
+        private By _botonPublicarBy = By.Id("PublicarResenya");
+        private By _botonModificarBocadillosBy = By.Id("ModificarBocadillos");
+
+        private By _errorsShownBy = By.Id("ErrorsShown");
+
+        private By _resumenResenyaBy = By.Id("ResumenResenya");
+        private By _tablaResumenBocadillosBy = By.Id("TableResumenBocadillos");
+
+
+        private IWebElement _nombreUsuario() => _driver.FindElement(_nombreUsuarioBy);
+        private IWebElement _tituloResenya() => _driver.FindElement(_tituloResenyaBy);
+        private IWebElement _descripcionResenya() => _driver.FindElement(_descripcionResenyaBy);
+        private IWebElement _valoracionGeneral() => _driver.FindElement(_valoracionGeneralBy);
+        private IWebElement _botonPublicar() => _driver.FindElement(_botonPublicarBy);
+        private IWebElement _botonModificarBocadillos() => _driver.FindElement(_botonModificarBocadillosBy);
+
+        public PostCrearResenya_PO(IWebDriver driver, ITestOutputHelper output)
+            : base(driver, output)
+        {
+        }
+
+        
+        // Rellenar formulario
+        
+
+        public void SetNombreUsuario(string nombre)
+        {
+            _nombreUsuario().SendKeys(nombre);
+        }
+
+        public void SetTituloResenya(string titulo)
+        {
+            _tituloResenya().SendKeys(titulo);
+        }
+
+        public void SetDescripcionResenya(string descripcion)
+        {
+            _descripcionResenya().SendKeys(descripcion);
+        }
+
+        public void SetValoracionGeneral(string estrellas)
+        {
+            var select = new SelectElement(_valoracionGeneral());
+            select.SelectByText(estrellas);
+        }
+
+
+        // Puntuación por bocadillo
+
+
+        public void SetPuntuacionBocadillo(string bocadilloId, string puntuacion)
+        {
+            var selectBy = By.Id(_puntuacionBocadilloPrefix + bocadilloId);
+            var select = new SelectElement(_driver.FindElement(selectBy));
+            select.SelectByText(puntuacion);
+        }
+
+        // Publicar reseña
+
+
+        public void PublicarResenya()
+        {
+            _botonPublicar().Click();
+            System.Threading.Thread.Sleep(300);
+        }
+
+        //  Modificar bocadillos (ESC-06)
+
+        public void ModificarBocadillos()
+        {
+            _botonModificarBocadillos().Click();
+            System.Threading.Thread.Sleep(300);
+        }
+
+        //  Botón publicar habilitado
+
+        public bool IsEnabledPublicar()
+        {
+            return _botonPublicar().Enabled;
+        }
+
+        // Mensajes de error (ESC-05)
+
+        public bool CheckMessageError(string errorMessage)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(_errorsShownBy);
+            _output.WriteLine($"Mensaje mostrado: {actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+
+        // Resumen de reseña (ESC-01)
+
+
+        public bool IsResumenResenyaVisible()
+        {
+            try
+            {
+                _driver.FindElement(_resumenResenyaBy);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool CheckResumenBocadillos(List<string[]> expectedBocadillos)
+        {
+            return CheckBodyTable(expectedBocadillos, _tablaResumenBocadillosBy);
+        }
+    }
+}
